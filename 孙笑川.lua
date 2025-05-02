8e0ae4a7d8ca2cc01b81afea38b9c6975642523a
SUNXIAOCHUAN886 <3598439173@qq.com>
2025-05-03 03:21:54 +0800

    Add files via upload

--- 孙笑川.lua
+++ 孙笑川.lua
@@ -1,13 +1,38 @@
-local success, library = pcall(function()
-    return loadstring(game:HttpGet("https://raw.githubusercontent.com/SUNXIAOCHUAN886/-/refs/heads/main/%E5%AD%99%E7%AC%91%E5%B7%9D.lua?token=GHSAT0AAAAAADDGWUMQSXNJKQYZG5NPNZL42AU5RWQ"))()
-end)
+local CoreGui = game:GetService("StarterGui")
 
-if not success then
-    warn("UI库加载失败")
-    return
-end
+CoreGui:SetCore("SendNotification", {
+    Title = "这是啥啊，迷你世界吗",
+    Text = "正在加载（反挂机已开启）",
+    Duration = 5, 
+})
+print("反挂机开启")
+		local vu = game:GetService("VirtualUser")
+		game:GetService("Players").LocalPlayer.Idled:connect(function()
+		   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+		   wait(1)
+		   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+		end)
+local OrionLib = loadstring(game:HttpGet("https://pastebin.com/raw/dUiCrXYP"))()
+local Window = OrionLib:MakeWindow({Name = "笑川逆天缝合", HidePremium = false, SaveConfig = true,IntroText = "大手子团队-笑川缝合", ConfigFolder = "欢迎大手子团队-笑川缝合"})
+local about = Window:MakeTab({
+    Name = "笑川缝合脚本",
+    Icon = "rbxassetid://93566402708060",
+    PremiumOnly = false
+})
 
-local window = library:new("孙笑川┋逆天缝合")
+about:AddParagraph("您的用户名:"," "..game.Players.LocalPlayer.Name.."")
+about:AddParagraph("您的注入器:"," "..identifyexecutor().."")
+about:AddParagraph("您当前服务器的ID"," "..game.GameId.."")
 
-local a = window:Tab("死铁轨", '16060333448')
-local b = a:section("设置", true)
+local Tab = Window:MakeTab({
+	Name = "惊喜大盲盒",
+	Icon = "rbxassetid://10527577695",
+	PremiumOnly = false
+})
+
+Tab:AddButton({
+  Name = "这是什么呢..？",
+  Callback = function()
+    game.Players.LocalPlayer:Kick("哈哈哈这是被踢出")
+  end
+})
