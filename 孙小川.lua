4db1fc5f42da784796f6a771aa174e5b5c136155
SUNXIAOCHUAN886 <3598439173@qq.com>
2025-05-03 01:56:15 +0800

    Add files via upload

---
+++ 孙小川.lua
@@ -0,0 +1,26 @@
+local StarterGui = game:GetService("StarterGui")
+local Players = game:GetService("Players")
+local WhitelistedPlayers = {
+    ["woainiyibeizi888"] = true,
+    ["htftgdg"] = true,
+}
+
+local function IsWhitelisted(player)
+    return WhitelistedPlayers[player.Name] or false
+end
+
+local localPlayer = Players.LocalPlayer
+
+local isLocalPlayerWhitelisted = IsWhitelisted(localPlayer)
+
+if isLocalPlayerWhitelisted then
+    StarterGui:SetCore("SendNotification", {
+        Title = "笑川验证中",
+        Text = "玩家:"..localPlayer.Name.."，检测完毕您是尊贵的笑川脚本用户，稍后会加载脚本",
+        Duration = 7,
+    })
+    script_key="PASTEKEYHERE";
+(loadstring or load)(game:HttpGet("https://getnative.cc/script/loader"))()
+else
+    localPlayer:Kick("你没有被加入白名单，请找3598439173购买")
+end
